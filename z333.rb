4f849459ed30a6f181091eca46db0909cb94b453
dmitriybilli <dmitriybilli@gmail.com>
2021-10-13 16:23:38 +0300

    New file

---
+++ z333.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class User
+  def fio
+    'Ivan Ivanov'
+  end
+
+  def profession
+    'Programmer'
+  end
+end
+
+class Computer
+end
+
+class Host
+end
+
+class Net
+end
+
+$stdout = File.open('user.txt', 'w')
+
+i = User.new
+puts "Имя #{i.fio}"
+puts "Профессия #{i.profession}"
+puts 'Профессия' + i.profession
